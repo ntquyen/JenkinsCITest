cb5579382f7ff090c483c8da15f2e1cc1b595f9d
ntquyen <nguyen.tuan.quyen.it@gmail.com>
2014-04-14 10:44:28 +0700

    remove delta task

--- Gruntfile.js
+++ Gruntfile.js
@@ -549,7 +549,7 @@ module.exports = function ( grunt ) {
    * before watching for changes.
    */
   grunt.renameTask( 'watch', 'delta' );
-  grunt.registerTask( 'watch', [ 'build', 'karma:unit', 'delta' ] );
+  grunt.registerTask( 'watch', [ 'build', 'karma:unit' ] );
 
   /**
    * The default task is to build and compile.
